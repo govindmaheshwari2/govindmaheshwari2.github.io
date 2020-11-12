0b260854ca3f7eac2db11b60d9a842f9f23a0ba4
Mukul Rathi <mukulr007@yahoo.co.in>
2020-11-13 01:24:42 +0530

    Ui changes

--- js/slider.js
+++ js/slider.js
@@ -7,67 +7,61 @@ var sliderHead2 = document.getElementById("sliderHead2");
 
 var current = 1;
 
-function sliderChange(select){
-if(select==1){
+function sliderChange(select) {
+  if (select == 1) {
     slider1.classList.add("active");
     slider2.classList.remove("active");
     slider3.classList.remove("active");
     slider4.classList.remove("active");
-    current=1;
+    current = 1;
     // image.src="images/how-it-worksks-2.png";
-   // sliderHead.innerHTML = "* Construction Package Finalised by Customer";
-    sliderHead1.innerHTML = "1.1 Construction Package Finalised by Customer";
-    sliderHead2.innerHTML = "1.2 Initial Booking & Advance Payment";
-}else if(select==2){
+    // sliderHead.innerHTML = "* Construction Package Finalised by Customer";
+    sliderHead1.innerHTML = "Construction Package Finalised by Customer";
+    sliderHead2.innerHTML = "Initial Booking & Advance Payment";
+  } else if (select == 2) {
     slider1.classList.add("active");
     slider2.classList.add("active");
     slider3.classList.remove("active");
     slider4.classList.remove("active");
-  current=2;
+    current = 2;
     // image.src="images/how-it-works-4.png";
-    sliderHead1.innerHTML = "2.1 Drawing, Design discussions & Finalizing";
-    sliderHead2.innerHTML = "2.2 Main Agreement signed";
-}else if(select==3){
+    sliderHead1.innerHTML = "Drawing, Design discussions & Finalizing";
+    sliderHead2.innerHTML = "Main Agreement signed";
+  } else if (select == 3) {
     slider1.classList.add("active");
     slider2.classList.add("active");
     slider3.classList.add("active");
     slider4.classList.remove("active");
- current=3;
+    current = 3;
     // image.src="images/how-it-works-5.png";
-    sliderHead1.innerHTML = "3.1 Project Schedule Creation & Excution";
-    sliderHead2.innerHTML = "3.2 Monthly Cash-flow created";
-}else if(select==4){
+    sliderHead1.innerHTML = "Project Schedule Creation & Excution";
+    sliderHead2.innerHTML = "Monthly Cash-flow created";
+  } else if (select == 4) {
     slider1.classList.add("active");
     slider2.classList.add("active");
     slider3.classList.add("active");
     slider4.classList.add("active");
-current=4;
+    current = 4;
     // image.src="images/how-it-works-3.png";
-    sliderHead1.innerHTML = "4.1 Progress tracking and audit";
-    sliderHead2.innerHTML = "4.2 Project completion & Handover";
-}
-}
-
-function next(){
-  if(current<4)
-  sliderChange(++current);
-}
-function previous(){
-  if(current>1)
-  sliderChange(--current);
+    sliderHead1.innerHTML = "Progress tracking and audit";
+    sliderHead2.innerHTML = "Project completion & Handover";
+  }
 }
 
+function next() {
+  if (current < 4) sliderChange(++current);
+}
+function previous() {
+  if (current > 1) sliderChange(--current);
+}
 
-$(document).ready(function() {
-    $('.collapse.in').prev('.panel-heading').addClass('active');
-    $('#accordion, #bs-collapse')
-      .on('show.bs.collapse', function(a) {
-        $(a.target).prev('.panel-heading').addClass('active');
-      })
-      .on('hide.bs.collapse', function(a) {
-        $(a.target).prev('.panel-heading').removeClass('active');
-      });
-  });
-
-
-    
+$(document).ready(function () {
+  $(".collapse.in").prev(".panel-heading").addClass("active");
+  $("#accordion, #bs-collapse")
+    .on("show.bs.collapse", function (a) {
+      $(a.target).prev(".panel-heading").addClass("active");
+    })
+    .on("hide.bs.collapse", function (a) {
+      $(a.target).prev(".panel-heading").removeClass("active");
+    });
+});
